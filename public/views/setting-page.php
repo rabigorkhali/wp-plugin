8ef2683644e0cc2923f87b20c61080906e6e89a2
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2024-03-20 12:31:02 +0545

    rrrrrsdfasdas

--- public/views/setting-page.php
+++ public/views/setting-page.php
@@ -8,6 +8,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- DataTables CSS -->
 <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css2" rel="stylesheet">
 
 <div class="wrap">
     <h2> Interest Creator Plugin</h2>
